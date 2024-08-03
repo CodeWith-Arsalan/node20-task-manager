6e38aebbceffacb5d20d657d6acd596432487580
CodeWith-Arsalan <arsalanshahid6655@gmail.com>
2024-08-03 15:12:12 +0300

    =Some new methods of mongodb and added mongoose for ease of use and handle some validation and also added validator package to use complex validations

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 // CRUD operations Create Read Update Delete
 
-const mongodb = require('mongodb')
-const mongoClient = mongodb.MongoClient
+//const mongodb = require('mongodb')
+const {MongoClient, ObjectId} = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -18,7 +18,7 @@ const databaseName = 'task-manager'
 
 async function connectToDatabase() {
     try {
-        const client = await mongoClient.connect(connectionURL);
+        const client = await MongoClient.connect(connectionURL);
         console.log("Connected Successfully");
         // You can add additional code to work with the database here if needed.
         const db = client.db(databaseName)
@@ -59,19 +59,70 @@ async function connectToDatabase() {
 
         // console.log(result)
 
-        const result = await collection.findOne({name: "sajid"})
+    //     const result = await collection.findOne({name: "sajid"})
 
-        if (result)
+    //     if (result)
+    //     {
+    //         console.log("User Found", result)
+    //     } else 
+    //     {
+    //         console.log("User not found")
+    //     }
+    
+
+    //  const find = await collection.find({age: 24})
+    //  const result = await find.toArray()
+    //  if (result.length > 0)
+    //  {
+    //     console.log("result: ",result)
+    //  }else{
+    //     console.log("Error finding user")
+    //  }
+    //console.log(result)
+
+    // const result = await collection.updateOne(
+    //     {
+    //         _id: new ObjectId("66a636ca6174a443f1739892")
+    //     },
+    //     {
+    //         $set: {
+    //             name: "Sultan Farhan"
+    //         }
+    //     }
+    // )
+
+    // if(result.matchedCount === 0)
+    // {
+    //     console.log("No user found with the ID")
+    // } else
+    // {
+    //     console.log("User Updated Successfully")
+    //     console.log(result)
+    // }
+
+    const result = await collectionTasks.updateMany(
         {
-            console.log("User Found", result)
-        } else 
+            completed: false
+        },
         {
-            console.log("User not found")
+            $set: {
+                completed: true
+            }
+        })
+
+        if(result.matchedCount === 0)
+        {
+            console.log("Unable to find the completed")
+        }else
+        {
+            console.log("Updated Successfully")
         }
-        client.close(); // Close the connection when done.
-    } catch (error) {
-        console.error('Unable to connect to database:', error);
-    }
+
+
+    client.close(); // Close the connection when done.
+} catch (error) {
+    console.error('Unable to connect to database:', error);
+}
 }
 
 connectToDatabase();
